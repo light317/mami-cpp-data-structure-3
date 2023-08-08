93339d9ef75e1cfb8d053da59d1cfb2cb3da2c94
Moussa317 <moussa.malli@ozbul.com>
2023-08-08 23:36:15 +0100

    Updated sleep

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,18 @@
 #include <iostream>
-#include <chrono>
-#include <thread>
 #include "FileReader.hpp"
+#include <windows.h>
 
 int main(){
 
     FileReader* fileReader = new FileReader("numbers.txt");
     TreeList* data = fileReader->GetTreeList();
     int sleepDuration = 10;
-    
+
     BST* t;
     for(int i = 0; i<data->GetSize();i++){
         t = data->GetTreeAtIndex(i);
         t->PrintPostOrder(t->root);       
         std::cout<<"\n";
-        std::this_thread::sleep_for(std::chrono::milliseconds(sleepDuration));
+        Sleep(sleepDuration);
     }
 }
